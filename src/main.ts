931a236da948e63b95d31fdb34242ce4d383e2a3
krishnadas710 <skrishnadas61@gmail.com>
2025-03-05 11:32:57 +0530

    env test complete and configured

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { TransformResponseInterceptor } from './common/interceptors/transform-re
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bodyParser: false,
