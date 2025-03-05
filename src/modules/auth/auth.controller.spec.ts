29fca73a37bcc238cce2f06424b803c7a9557f1f
krishnadas710 <skrishnadas61@gmail.com>
2025-03-05 11:37:37 +0530

    auth part is completed

--- src/modules/auth/auth.controller.spec.ts
+++ src/modules/auth/auth.controller.spec.ts
@@ -5,9 +5,11 @@ describe('AuthController', () => {
   let controller: AuthController;
 
   beforeEach(async () => {
+    
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
     }).compile();
+
 
     controller = module.get<AuthController>(AuthController);
   });
